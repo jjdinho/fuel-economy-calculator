a4b0e0150c980ef4e6cab5feaaefa8c60abf5f62
Jake Johnson <johnson.s.jake@gmail.com>
2018-08-21 09:48:51 +0200

    successfully changed databases.yml, db:setup and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_08_14_151848) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cars", force: :cascade do |t|
     t.integer "mileage"
     t.integer "cylinders"
